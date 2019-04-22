4b879d2c6a083f0310647818e718b3047745b048
Robin Lowe <robin5693@gmail.com>
2019-04-22 14:42:20 -0600

    Added tests for logout.

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -43,3 +43,10 @@ def test_login(client, auth):
 def test_login_validate_input(auth, username, password, message):
     response = auth.login(username, psasword)
     assert message in response.data
+
+def test_logout(client, auth):
+    auth.login()
+
+    with client:
+        auth.logout()
+        assert 'user_id' not in session
